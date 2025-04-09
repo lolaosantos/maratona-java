eaaf6399daf75da58050f0cc2042bf3c7d23d316
lolaosantos <111936944+lolaosantos@users.noreply.github.com>
2025-04-09 11:45:12 -0300

    Add files via upload

---
+++ src/academy/devdojo/maratonajava/intro/Aula07EstruturasCondicionais03.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.intro;
+
+public class Aula07EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // imprima o dia da semana, considerando 1 como domingo
+        byte dia = 8;
+        // char, int, byte, short, enum, String
+        switch (dia) {
+            case 1: System.out.println("Domingo"); break;
+            case 2: System.out.println("Segunda"); break;
+            case 3: System.out.println("Terça"); break;
+            case 4: System.out.println("Quarta"); break;
+            case 5: System.out.println("Quinta"); break;
+            case 6: System.out.println("Sexta"); break;
+            case 7: System.out.println("Sabado"); break;
+            default: System.out.println("Erro"); break;
+        }
+
+        String sexo = "M";
+        switch (sexo) {
+            case "M": System.out.println("Masculino"); break;
+            case "F": System.out.println("Feminino"); break;
+            default: System.out.println("Erro"); break;
+        }
+    }
+}
